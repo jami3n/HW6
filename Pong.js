09cfea28332e7aaf64c7c139bbb358c12de94d55
jami3n <60486459+jami3n@users.noreply.github.com>
2020-03-07 14:16:29 -0800

    Create Pong.js

---
+++ Pong.js
@@ -0,0 +1,118 @@
+// Variables
+var playerHeight = 80
+var playerWidth = 20
+var playerSpeed = 8
+var playerL = 200
+var playerR = 200
+
+var scoreL = 0
+var scoreR = 0
+
+var ballX = 300
+var ballY = 200
+var ballSize = 20
+var ballXSpeed = 4
+var ballYSpeed = -2
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0, 0, 50, 100);
+  
+  // draw left player
+  noStroke();
+  fill(150, 200, 255)
+  rect(0, playerL, playerWidth, playerHeight);
+  
+  // draw right player
+  rect(width-playerWidth, playerR, playerWidth, playerHeight);
+  
+  // draw ball
+  fill(220, 220, 10)
+  ellipse(ballX, ballY, ballSize)
+  
+  // draw score
+  fill(255);
+  textSize(20);
+  text(scoreL, 150, 30);
+  text(scoreR, 450, 30);
+  
+  
+  /* User Input */
+  // 'W' key
+  if (keyIsDown(87)) {
+    playerL = playerL - playerSpeed
+  }
+  // 'S' key
+  if (keyIsDown(83)) {
+    playerL = playerL + playerSpeed
+  }
+  
+  if (keyIsDown(UP_ARROW)) {
+    playerR = playerR - playerSpeed
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    playerR = playerR + playerSpeed
+  }
+  
+  /* Game logic */
+  if (playerL <= 0) {
+    playerL = 0;
+  }
+  if (playerL > height - playerHeight) {
+    playerL = height - playerHeight;
+  }
+  
+  if (playerR <= 0) {
+    playerR = 0;
+  }
+  if (playerR > height - playerHeight) {
+    playerR = height - playerHeight;
+  }
+  
+  ballX = ballX + ballXSpeed
+  ballY = ballY + ballYSpeed
+  
+  // Bounce off top wall
+  if (ballY < 0) {
+    ballY = 0;
+    ballYSpeed = -ballYSpeed;
+  }
+
+  // Bounce off bottom wall
+  if (ballY > height) {
+    ballY = height;
+    ballYSpeed = -ballYSpeed;
+  }
+  
+  
+  // bounce off right player
+  if (ballX > width - playerWidth && ballY >= playerR && ballY <= playerR + playerHeight) {
+    ballX = width - playerWidth
+    ballXSpeed = -ballXSpeed
+  }
+  
+  // playerL scores!
+  if (ballX > width) {
+    ballX = width/2
+    ballY = height/2
+    scoreL = scoreL + 1
+    ballXSpeed = - ballXSpeed 
+  }
+  
+   // bounce off left player
+  if (ballX < 0 + playerWidth && ballY >= playerL && ballY <= playerL + playerHeight) {
+    ballX = 0 + playerWidth
+    ballXSpeed = -ballXSpeed
+  }
+  
+  // playerR scores!
+  if (ballX < 0) {
+    ballX = width/2
+    ballY = height/2
+    scoreR = scoreR + 1
+    ballXSpeed = - ballXSpeed 
+  }
+}
